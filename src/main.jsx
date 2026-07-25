ab9657c4fd6409938863cf800b433df0c3c08540
no_one <135714413+amrmohamed125@users.noreply.github.com>
2026-07-25 05:42:45 +0300

    fix base path and vercel routing

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-grocery-app">
+    <BrowserRouter>
       <CartProvider>
         <App />
       </CartProvider>
