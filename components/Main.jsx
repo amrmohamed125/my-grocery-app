b04a75d9a2b504f6510890e47f81eddfd97fa778
no_one <135714413+amrmohamed125@users.noreply.github.com>
2026-07-26 20:55:43 +0300

    fix duplicate data variable in Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -34,7 +34,7 @@ function Main() {
             try {
                 setLoading(true);
                 const response = await fetch('/api/products?page=home_page');
-                const data = await response.json();
+                // const result = await response.json();
                 if (!response.ok) {
                     throw new Error('Failed to fetch popular products');
                 }
